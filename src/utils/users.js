549460ca01cb8942160438531dfdd8b938b4c54a
VenireVidereVincere <alan_jurado_canseco@hotmail.com>
2022-08-24 10:01:52 -0500

    Init commit

---
+++ src/utils/users.js
@@ -0,0 +1,54 @@
+const users = []
+
+// Create new user when joining room 
+const addUser = ({id,username,room})=>{
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+    // Validate the data
+    if(!room||!username){
+        return {
+            error: "Username and room are required"
+        }
+    }
+    // Check for existing user
+    const existingUser = users.find((user)=>{
+        return user.room===room && user.username === username
+    })
+    if (existingUser){
+        return {
+            error:"Username is in use!"
+        }
+    }
+    // Store user
+    const user = {id,username,room}
+    users.push(user)
+    return {user}
+}
+
+// Remove user
+const removeUser = (id)=>{
+    const index = users.findIndex((user)=> user.id === id)
+
+    if(index!==-1){
+        return users.splice(index,1)[0]
+    }
+}
+
+// Returns the user object from the array
+const getUser = (id)=>{
+    const index = users.findIndex((user)=>user.id === id)
+    return users[index]
+}
+
+// Returns the users in a room
+const getUsersInRoom=(room)=>{
+    return users.filter((user)=>user.room===room.trim().toLowerCase())
+}
+
+module.exports=({
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+})
